b3a22012be44d2ee902fb635c156f47727544246
overlordmimi <103212272+overlordmimi@users.noreply.github.com>
2022-05-29 16:27:54 -0700

    Add files via upload
    I genuinely forgot about making a github repo, so this is the completed projected all pushed in one go. I am not too sure how github works, so this might be able to show all my edits, but in case it does not, this project took me about 20 hours to make

---
+++ src/scenes/Menu.js
@@ -0,0 +1,60 @@
+class Menu extends Phaser.Scene {
+    constructor() {
+        super("menuScene");
+    }
+
+    preload() {
+        // load audio
+        this.load.audio('sfx_select', './assets/blip_select12.wav');
+        this.load.audio('sfx_explosion', './assets/explosion38.wav');
+        this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
+    }
+
+    create() {
+        // menu text configuration
+        let menuConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#edabc8',
+            color: '#843605',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
+        
+        // show menu text
+        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'FLOWER BLOOM', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to water', menuConfig).setOrigin(0.5);
+        menuConfig.backgroundColor = '#6c854c';
+        menuConfig.color = '#edabc8';
+        this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← or → to play', menuConfig).setOrigin(0.5);
+
+        // define keys
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+    }
+
+    update() {
+        if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+          // Novice mode
+          game.settings = {
+            spaceshipSpeed: 1,
+            gameTimer: 60000    
+          }
+          this.sound.play('sfx_select');
+          this.scene.start("playScene");    
+        }
+        if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
+          // Expert mode
+          game.settings = {
+            spaceshipSpeed: 4,
+            gameTimer: 45000    
+          }
+          this.sound.play('sfx_select');
+          this.scene.start("playScene");    
+        }
+      }
+}
